15522cf91b11ddc04ca631995635e7ff725a1513
Farzin Jabbary <farzin@Farzins-MacBook-Air.local>
2025-05-17 12:33:03 -0700

    added other table cell values for task management

--- client/src/pages/TaskManagement.tsx
+++ client/src/pages/TaskManagement.tsx
@@ -1,13 +1,39 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { initialMockTasks } from '../data/data';
 import Table from 'react-bootstrap/Table';
-import type { Task } from '../data/type';
+import type { Task, User } from '../data/type';
 import Badge from 'react-bootstrap/Badge';
 
 function TaskManagement() {
   const [tasks, setTasks] = useState<Task[]>(initialMockTasks)
-  const headers = Object.keys(tasks[0]).filter(key => key !== 'progress' && key !== 'description' && key !== 'priority') as (keyof Task)[]
-  console.log(headers);
+  const [users, setUsers] = useState<User[]>([])
+
+  useEffect(() => {
+    fetch('http://localhost:3000/api/users')
+    .then(res => res.json())
+    .then(data => setUsers(data))
+  }, [])
+
+  console.log('tasks==>');
+  console.log(tasks);
+  console.log('users==>');
+  console.log(users);
+
+  const initialHeaders = tasks.length > 0 ? Object.keys(tasks[0]).filter(key => key !== 'progress' && key !== 'description' && key !== 'priority' && key !== 'storyPoints' && key !== 'createdAt') as (keyof Task)[] : [];
+
+  const [headers, _] = useState(() => {
+    return initialHeaders.map(header => {
+      if (header === 'documentationLink') {
+        return 'Documentation';
+      }
+      else if (header === 'dueDate') {
+        return 'Due Date'
+      } else if (header === 'updatedAt') {
+        return 'Updated At';
+      }
+      return header;
+    });
+  });
 
   function formatDate(dateString: string | number | Date) {
     const date = new Date(dateString);
@@ -20,7 +46,7 @@ function TaskManagement() {
       <thead>
         <tr>
           {headers.map((header) => (
-            <th key={header}>{header.charAt(0).toUpperCase() + header.slice(1)}</th>
+            <th key={header} style={{ whiteSpace: 'nowrap' }}>{header.charAt(0).toUpperCase() + header.slice(1)}</th>
           ))}
         </tr>
       </thead>
@@ -34,6 +60,7 @@ function TaskManagement() {
             </td>
             <td>
               <Badge
+              style={{ fontWeight: 'normal' }}
               bg={
                 task.status === 'Done'
                   ? 'success'
@@ -45,11 +72,13 @@ function TaskManagement() {
               }
               >{task.status}</Badge>
             </td>
-            <td style={{ whiteSpace: 'nowrap' }}>{task.dueDate ? formatDate(task.dueDate) : 'N/A'}</td>
+            <td style={{ whiteSpace: 'nowrap' }}><small> {task.dueDate ? formatDate(task.dueDate) : 'N/A'}</small></td>
+            <td> </td>
             <td>
-              
+              {task.tags.map(tag => ( <Badge bg="light" className='m-1'  style={{ fontWeight: 'normal', color: 'black' }}>{tag}</Badge> ))}
             </td>
-
+            <td>Updated at</td>
+            <td> 📂 <a target='_blank' style={{textDecoration: 'none'}} href={task.documentationLink}>{task.documentationLink.substring(task.documentationLink.lastIndexOf('/') + 1)}</a> </td>
 
 
 
