00f1bfc8611bb289d05e948d863d1a54af3f8ee7
Farzin Jabbary <farzin@Farzins-MacBook-Air.local>
2025-05-17 19:22:01 -0700

    added edit modal component and passed the props

--- client/src/pages/TaskManagement.tsx
+++ client/src/pages/TaskManagement.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect, useRef } from 'react';
+import { useState, useEffect, useRef, Fragment } from 'react';
 import { initialMockTasks } from '../data/data';
 import Table from 'react-bootstrap/Table';
 import type { Task, User } from '../data/type';
@@ -12,18 +12,23 @@ import Stack from 'react-bootstrap/Stack';
 import { UserImg } from '../styled';
 import { toast } from 'react-toastify';
 import Overlay from 'react-bootstrap/Overlay';
+import EditModal from '../components/EditModal';
 
 function TaskManagement() {
   const [tasks, setTasks] = useState<Task[]>(initialMockTasks)
   const [users, setUsers] = useState<User[]>([])
   const [showAssignees, setShowAssignees] = useState(false);
   const [showTooltip, setShowTooltip] = useState(false);
+  const [showModal, setShowModal] = useState(false);
+  const [selectedTask, setSelectedTask] = useState<Task>({} as Task);
+
   const target = useRef(null);
 
   useEffect(() => {
     fetch('http://localhost:3000/api/users')
       .then(res => res.json())
       .then(data => setUsers(data))
+      .catch(err => console.error('Error fetching users:', err));
   }, [])
 
   // console.log('tasks==>');
@@ -34,18 +39,18 @@ function TaskManagement() {
   const initialHeaders = tasks.length > 0 ? Object.keys(tasks[0]).filter(key => key !== 'progress' && key !== 'description' && key !== 'priority' && key !== 'storyPoints' && key !== 'createdAt') as (keyof Task)[] : [];
 
   const [headers, _] = useState(() => {
-      return initialHeaders.map(header => {
-        if (header === 'documentationLink') {
-          return { display: 'Documentation', key: 'documentationLink' };
-        }
-        else if (header === 'dueDate') {
-          return { display: 'Due Date', key: 'dueDate' };
-        } else if (header === 'updatedAt') {
-          return { display: 'Updated At', key: 'updatedAt' };
-        }
-        return { display: header.charAt(0).toUpperCase() + header.slice(1), key: header };
-      });
+    return initialHeaders.map(header => {
+      if (header === 'documentationLink') {
+        return { display: 'Documentation', key: 'documentationLink' };
+      }
+      else if (header === 'dueDate') {
+        return { display: 'Due Date', key: 'dueDate' };
+      } else if (header === 'updatedAt') {
+        return { display: 'Updated At', key: 'updatedAt' };
+      }
+      return { display: header.charAt(0).toUpperCase() + header.slice(1), key: header };
     });
+  });
 
   function formatDate(dateString: string | number | Date) {
     const date = new Date(dateString);
@@ -77,7 +82,7 @@ function TaskManagement() {
       prevTasks.map(item => {
         if (item.id === task.id) {
           const updatedTask = { ...item, assignee: item.assignee?.filter(assignee => assignee.id !== user.id) };
-          toast.error(`${user.name} removed from assignee`);   
+          toast.error(`${user.name} removed from assignee`);
           return updatedTask;
         }
         return item;
@@ -86,7 +91,7 @@ function TaskManagement() {
   };
 
   // Sorts the tasks by the selected header
-  const handleSortById = (header: keyof Task) =>  {
+  const handleSortById = (header: keyof Task) => {
     const sortedTasks = [...tasks].sort((a, b) => {
       if (header === 'id') {
         return a.id - b.id;
@@ -104,91 +109,100 @@ function TaskManagement() {
     setTasks(sortedTasks);
     toast.info(`Sorted by ${header.charAt(0).toUpperCase() + header.slice(1)}`);
   }
-  
+
+  const editTask = (task: Task) => {
+    console.log(task)
+    setShowModal(true);
+    setSelectedTask(task);
+  }
+
   return (
-    <Table bordered hover>
-      <thead>
-        <tr>
-          {headers.map(({ display, key }) => (
-            <th key={key} style={{ whiteSpace: 'nowrap' }} onClick={() => handleSortById(key as keyof Task)}>{display} <small style={{fontSize: '12px'}}> ↕️</small></th>
-          ))}
-        </tr>
-      </thead>
-      <tbody>
-        {tasks.map(task => (
-          <tr key={task.id}>
-            <td>{task.id}</td>
-            <td>
-              <p className='m-0'>{task.title}</p>
-              <small className='text-muted'>{task.description}</small>
-            </td>
-            <td>
-              <Badge
-                style={{ fontWeight: 'normal' }}
-                bg={
-                  task.status === 'Done'
-                    ? 'success'
-                    : task.status === 'In Progress'
-                      ? 'warning'
-                      : task.status === 'Blocked'
-                        ? 'danger'
-                        : 'primary'
-                }
-              >{task.status}</Badge>
-            </td>
-            <td style={{ whiteSpace: 'nowrap' }}><small> {task.dueDate ? formatDate(task.dueDate) : 'N/A'}</small></td>
-            <td>
-              {[DropdownButton].map((DropdownType, idx) => (
-                <DropdownType
-                  as={ButtonGroup}
-                  key={idx}
-                  id={`dropdown-button-drop-${idx}`}
-                  size="sm"
-                  variant="info"
-                  title="Users"
-                >
-                  {
-                    users.map(user => (
-                      <Stack direction="horizontal" gap={3} key={user.id}>
-                        <Dropdown.Item eventKey={user.id} onClick={() => handleAddAssignee(task, user)}>
-                          <UserImg src={user.imgURL ? user.imgURL : 'https://www.shutterstock.com/image-vector/grey-person-icon-business-vector-260nw-2178945117.jpg'} alt={user.name} />
-                          <span>{user.name}</span>
-                        </Dropdown.Item>
-                      </Stack>
-                    ))
-                  }
-                </DropdownType>
-              ))}
-
-              {task.assignee.length === 1 && <div style={{ whiteSpace: 'nowrap' }}><small>{task.assignee[0].name}</small></div>}
-              {task.assignee.length > 1 && (
-                <div>{task.assignee[0].name} <Badge style={{ cursor: 'pointer' }} ref={target} bg='success' onClick={() => setShowTooltip(!showTooltip)}>  +{task.assignee.length - 1} </Badge> </div>
-              )}
-              <Overlay target={target.current} show={showTooltip} placement="right" >
-                {(props) => (
-
-                  <StyledTooltip id="overlay-example" {...props}>
-                    {task.assignee.slice(1,).map(assignee => (
-                      <div key={assignee.id} className='d-flex align-items-center justify-content-between p-1 my-3' style={{borderRight: '3px solid green', boxShadow: '0 0 5px rgba(0,0,0,0.5)'}}>
-                        <div className='m-1 d-flex' style={{ fontWeight: 'normal', color: 'black' }}> <UserImg src={assignee.imgURL ? assignee.imgURL : 'https://www.shutterstock.com/image-vector/grey-person-icon-business-vector-260nw-2178945117.jpg'} alt={assignee.name} /> <span style={{ whiteSpace: 'nowrap' }}>{assignee.name}</span></div>
-                        <span style={{ cursor: 'pointer' }} className='p-1 text-danger' onClick={() => handleDeleteAssignee(task, assignee)}>X</span>
-                      </div>
-                    ))}
-                  </StyledTooltip>
-                )}
-              </Overlay>
-
-            </td>
-            <td>
-              {task.tags.map((tag, index) => (<Badge key={index} bg="light" className='m-1' style={{ fontWeight: 'normal', color: 'black' }}>{tag}</Badge>))}
-            </td>
-            <td><small>{formatDate(task.updatedAt)}</small></td>
-            <td> 📂 <a target='_blank' style={{ textDecoration: 'none' }} href={task.documentationLink}>{task.documentationLink.substring(task.documentationLink.lastIndexOf('/') + 1)}</a> </td>
-
+    <Fragment>
+      <Table bordered hover>
+        <thead>
+          <tr>
+            {headers.map(({ display, key }) => (
+              <th key={key} style={{ whiteSpace: 'nowrap' }} onClick={() => handleSortById(key as keyof Task)}>{display} <small style={{ fontSize: '12px' }}> ↕️</small></th>
+            ))}
           </tr>
-        ))}
-      </tbody>
-    </Table>
+        </thead>
+        <tbody>
+          {tasks.map(task => (
+            <tr key={task.id} onClick={() => editTask(task)}>
+              <td>{task.id}</td>
+              <td>
+                <p className='m-0'>{task.title}</p>
+                <small className='text-muted'>{task.description}</small>
+              </td>
+              <td>
+                <Badge
+                  style={{ fontWeight: 'normal' }}
+                  bg={
+                    task.status === 'Done'
+                      ? 'success'
+                      : task.status === 'In Progress'
+                        ? 'warning'
+                        : task.status === 'Blocked'
+                          ? 'danger'
+                          : 'primary'
+                  }
+                >{task.status}</Badge>
+              </td>
+              <td style={{ whiteSpace: 'nowrap' }}><small> {task.dueDate ? formatDate(task.dueDate) : 'N/A'}</small></td>
+              <td>
+                {[DropdownButton].map((DropdownType, idx) => (
+                  <DropdownType
+                    as={ButtonGroup}
+                    key={idx}
+                    id={`dropdown-button-drop-${idx}`}
+                    size="sm"
+                    variant="info"
+                    title="Users"
+                  >
+                    {
+                      users.map(user => (
+                        <Stack direction="horizontal" gap={3} key={user.id}>
+                          <Dropdown.Item eventKey={user.id} onClick={() => handleAddAssignee(task, user)}>
+                            <UserImg src={user.imgURL ? user.imgURL : 'https://www.shutterstock.com/image-vector/grey-person-icon-business-vector-260nw-2178945117.jpg'} alt={user.name} />
+                            <span>{user.name}</span>
+                          </Dropdown.Item>
+                        </Stack>
+                      ))
+                    }
+                  </DropdownType>
+                ))}
+
+                {task.assignee.length === 1 && <div style={{ whiteSpace: 'nowrap' }}><small>{task.assignee[0].name}</small></div>}
+                {task.assignee.length > 1 && (
+                  <div>{task.assignee[0].name} <Badge style={{ cursor: 'pointer' }} ref={target} bg='success' onClick={() => setShowTooltip(!showTooltip)}>  +{task.assignee.length - 1} </Badge> </div>
+                )}
+                <Overlay target={target.current} show={showTooltip} placement="right" >
+                  {(props) => (
+
+                    <StyledTooltip id="overlay-example" {...props}>
+                      {task.assignee.slice(1,).map(assignee => (
+                        <div key={assignee.id} className='d-flex align-items-center justify-content-between p-1 my-3' style={{ borderRight: '3px solid green', boxShadow: '0 0 5px rgba(0,0,0,0.5)' }}>
+                          <div className='m-1 d-flex' style={{ fontWeight: 'normal', color: 'black' }}> <UserImg src={assignee.imgURL ? assignee.imgURL : 'https://www.shutterstock.com/image-vector/grey-person-icon-business-vector-260nw-2178945117.jpg'} alt={assignee.name} /> <span style={{ whiteSpace: 'nowrap' }}>{assignee.name}</span></div>
+                          <span style={{ cursor: 'pointer' }} className='p-1 text-danger' onClick={() => handleDeleteAssignee(task, assignee)}>X</span>
+                        </div>
+                      ))}
+                    </StyledTooltip>
+                  )}
+                </Overlay>
+
+              </td>
+              <td>
+                {task.tags.map((tag, index) => (<Badge key={index} bg="light" className='m-1' style={{ fontWeight: 'normal', color: 'black' }}>{tag}</Badge>))}
+              </td>
+              <td><small>{formatDate(task.updatedAt)}</small></td>
+              <td> 📂 <a target='_blank' style={{ textDecoration: 'none' }} href={task.documentationLink}>{task.documentationLink.substring(task.documentationLink.lastIndexOf('/') + 1)}</a> </td>
+
+            </tr>
+          ))}
+        </tbody>
+      </Table>
+      <EditModal showModal={showModal} setShowModal={setShowModal} selectedTask={selectedTask} />
+    </Fragment>
   )
 }
 
